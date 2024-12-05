4e1ab77d82c8f91299b24a6deb9db8b70dfa9fa0
ZR <sqlpractice19@gmail.com>
2024-12-05 10:29:34 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI_Project',
+    version= '0.0.0',
+    author= 'ZR',
+    author_email= 'sqlpractice19@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
